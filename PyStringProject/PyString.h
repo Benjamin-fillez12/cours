7c0eabbf6cbd187eb73c9f823adb96fdbb3c643d
Benjamin <benjamin.fillez@student-oec.fr>
2025-01-16 12:21:36 +0100

    Create PyString.h

---
+++ PyStringProject/PyString.h
@@ -0,0 +1,12 @@
+#ifndef StringModule
+	#define StringModule "Initialized"
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <string.h>
+	#include <stdbool.h>
+	#include <ctype.h>
+	char* _capitalize(char*);
+	const bool _islower(char*);
+	const bool _isUpper(char*);
+	char* _swapCase(char*);
+#endif

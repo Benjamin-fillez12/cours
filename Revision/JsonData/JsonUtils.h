10d5a9385a9528cda7c0177d0d9ec2a02bd84cdb
Benjamin <benjamin.fillez@student-oec.fr>
2025-03-07 13:13:59 +0100

    Update JsonUtils.h

--- Revision/JsonData/JsonUtils.h
+++ Revision/JsonData/JsonUtils.h
@@ -5,46 +5,6 @@
 
 #include <cJSON.h>
 
-/*
-    Reading json data:
-    {
-        char *json_str = read_file("test.json");
-        if(!json_str){
-            perror("Failed to open file");
-            free(json_str);
-            exit(1);
-        }
-        cJSON *json = cJSON_Parse(json_str);
-        if(!json){
-            perror("Parsing json failed.");
-            free(json_str);
-            exit(1);
-        }
-        cJSON *data = cJSON_GetObjectItem(json,"data");
-        cJSON *name = cJSON_GetObjectItem(data,"name");
-        if(cJSON_IsString(name)){
-            printf("Nom: %s\n",name->valuestring);
-        }
-        cJSON_Delete(json);
-        free(json_str);
-    }
-    Add json data:
-    {
-        cJSON *root = cJSON_CreateObject();
-        cJSON *data = cJSON_CreateObject();
-        cJSON_AddStringToObject(data,"name","Benjamin");
-        cJSON_AddItemToObject(root,"data",data);
-        char *json_str = cJSON_Print(root);
-        if(!__WriteData("test.json",json_str)){
-            printf("Writing data failed.\n");
-        }
-        else{
-            printf("Writing data is completed\n");
-        }
-        cJSON_Delete(root);
-    }
- */
-
 char *__ReadJSONFIle(const char *filename){
     FILE *file = fopen(filename,"r");
     if(!file){
